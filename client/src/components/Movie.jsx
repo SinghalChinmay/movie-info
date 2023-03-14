e9e68bc6ef99b714ab7fd7f101c1756d968d85dc
Chinmay Singhal <chinmay.singhal08@gmail.com>
2023-03-14 23:32:22 +0530

    feat: Completed most of the part of the project
    Will slowly keep adding more feature like pagination!

--- client/src/components/Movie.jsx
+++ client/src/components/Movie.jsx
@@ -1,6 +1,21 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
+function Genre({name}) {
+	return (
+		<div className="badge badge-primary">{name}</div>
+	)
+}
+
+function Genres({genres}) {
+	const genreList = genres.map((genre) => <Genre key={genre['id']} name={genre['name']}/>)
+	return (
+		<div className='flex gap-3'>
+			{genreList}
+		</div>
+	)
+}
+
 export default function Movie({ q, tv_or_movie }) {
 
 	let [isLoading, setIsLoading] = useState(false)
@@ -31,6 +46,7 @@ export default function Movie({ q, tv_or_movie }) {
 						<div className='movie-info place-items-center flex flex-col border-2 items-center max-w-[800px] border-solid mx-auto my-4 border-gray-500 p-3 text-xl gap-4 rounded-xl justify-center text-center'>
 							<img src={"https://image.tmdb.org/t/p/original" + data[0]['poster_path']} alt="Movie poster image" className='w-[650px] my-4 rounded-2xl h-[450px]'/>
 							<h1 className='text-3xl font-bold'>{tv_or_movie == 'tv' ? (data[0]['name']) : (data[0]['title'])}</h1>
+							<Genres genres={data[0]['genres']}/>
 							<p className='text-left p-5'>{data[0]['overview']}</p>
 							<div className="stats stats-vertical lg:stats-horizontal">
 								<div className="stat">
@@ -45,6 +61,20 @@ export default function Movie({ q, tv_or_movie }) {
 									<div className="stat-title">Original Language</div>
 									<div className="stat-value">{data[1]['language']}</div>
 								</div> 
+							</div>
+							<div className="stats stats-vertical lg:stats-horizontal">
+								<div className="stat">
+									<div className="stat-title">Popularity</div>
+									<div className="stat-value">{data[0]['popularity']}</div>
+								</div>
+								<div className="stat">
+									<div className="stat-title">Vote Average</div>
+									<div className="stat-value">{data[0]['vote_average']}</div>
+								</div>
+								<div className="stat">
+									<div className="stat-title">{tv_or_movie == 'tv' ? "Total Seasons" : "Runtime"}</div>
+									<div className="stat-value">{tv_or_movie == 'tv' ? (data[0]['number_of_seasons']) : (`${(data[0]['runtime']/60).toFixed(1)} hrs`)}</div>
+								</div>
 							</div>
 						</div>
 				)
